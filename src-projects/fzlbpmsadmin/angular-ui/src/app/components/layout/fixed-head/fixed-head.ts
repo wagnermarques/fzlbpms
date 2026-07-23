db1dbf4196422fc07487b5bd740974572cc99fc9
wagnermarques <wagnerdocri@gmail.com>
2026-07-23 00:45:32 -0300

    Add user logged indication at proper widget in the tauri app view

--- src-projects/fzlbpmsadmin/angular-ui/src/app/components/layout/fixed-head/fixed-head.ts
+++ src-projects/fzlbpmsadmin/angular-ui/src/app/components/layout/fixed-head/fixed-head.ts
@@ -1,17 +1,20 @@
 import { Component, Input } from '@angular/core';
 import { MatDrawer } from '@angular/material/sidenav';
-
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-fixed-head',
   standalone: true,
-  imports: [MatToolbarModule, MatIconModule, MatButtonModule],
+  imports: [MatToolbarModule, MatIconModule, MatButtonModule, MatTooltipModule],
   templateUrl: './fixed-head.html',
   styleUrl: './fixed-head.css',
 })
 export class FixedHead {
   @Input() drawer!: MatDrawer;
+
+  constructor(readonly auth: AuthService) {}
 }

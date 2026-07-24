cd4f3ff710682d8cd68a568a94c9175e705c9c0b
wagnermarques <wagnerdocri@gmail.com>
2026-07-24 00:37:25 -0300

    added fzlbpmsadmin static angular static web served throuhg nginx

---
+++ src-projects/fzlbpmsadmin-web/src/app/app.config.ts
@@ -0,0 +1,16 @@
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideHttpClient } from '@angular/common/http';
+
+import { routes } from './app.routes';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideAnimations(),
+    provideBrowserGlobalErrorListeners(),
+    provideZonelessChangeDetection(),
+    provideRouter(routes),
+    provideHttpClient()
+  ]
+};
